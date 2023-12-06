bc3a835af748224d593ccf9d9145a8a7d443785a
VanMendesvs <v_mendesv@hotmail.com>
2023-12-06 13:35:12 -0300

    Tarefa1JavaScript

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Ano novo está chegando!!!")
+
+let singer = "Mariah Carey"
+let ageSinger = 54
+let year = 2023 - ageSinger
+
+let aboutSinger = `A cantora que se apresentara sera: ${singer} ela tem ${ageSinger} anos e nasceu no ano ${year}`
+console.log(aboutSinger)
+
+console.log("O ano de nascimento da Mariah é:"  + year)
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
+
+let age = 29
+
+if (age >= 18) {    
+    console.log("Você é maior de idade, podera curtir o show do Roberto Carlos feat Mariah Carey")
+} else {
+    console.log("Sinto muito, mas assistirá da TV Globo")
+}
+    
+let month = `JUNHO`
+
+if (month === `Agosto`) {
+    console.log("O mês escolhido foi o mês:" + month)
+} else if (month === `Dezembro`) {
+    console.log("O mês escolhido foi o mês:" + month)
+} else if(month === `JUNHO` || month === 'Junho') {
+    console.log("O mês escolhido foi o mes:" + month)
+} else {
+    console.log("Algo de errado não está certo, o mês digitado foi o mês:" + month)
+}
+
+
+
+
